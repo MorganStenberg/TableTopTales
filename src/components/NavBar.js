2f15a8c3c8c7c973c2f87441f463f903133c8131
Morgan Stenberg <morganstenberg@gmail.com>
2024-04-04 07:33:35 +0000

    add logo and styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,46 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import styles from "../styles/NavBar.module.css";
+import logo from '../assets/logo_ttt.PNG'
 
 const NavBar = () => {
     return (
-      <Navbar expand="md" fixed="top">
+      <Navbar 
+      expand="md" 
+      fixed="top"
+      className={styles.NavBar}
+      >
         <Container>
           <Navbar.Brand>
+            <Image
+            roundedCircle
+            src={logo} 
+            alt='logo' 
+            height='45' 
+            />
             
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-left">
-              <Nav.Link>
-                <i className="fas fa-home"></i>Home
+            <Nav className="mr-auto text-left">
+              <Nav.Link
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              >
+                Home
               </Nav.Link>
-              <Nav.Link>
-                <i className="fas fa-sign-in-alt"></i>Sign in
+              <Nav.Link
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              >
+                Sign in
               </Nav.Link>
-              <Nav.Link>
-                <i className="fas fa-user-plus"></i>Sign up
+              <Nav.Link
+              className={styles.NavLink}
+              activeClassName={styles.Active}>
+                Sign up
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
