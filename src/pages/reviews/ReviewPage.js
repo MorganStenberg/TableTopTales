c47db4ccb4a9197443b56894abe78763bdffc047
Morgan Stenberg <morganstenberg@gmail.com>
2024-04-08 11:56:20 +0000

    add function to fetch reviews

--- src/pages/reviews/ReviewPage.js
+++ src/pages/reviews/ReviewPage.js
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 
 
 
 function ReviewPage() {
-    
+    const { id } = useParams();
+    const [review, setReview] = useState({results: [] });
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{data: review}] = await Promise.all([
+                    axiosReq.get(`/reviews/${id}`)
+                ])
+                setReview({results: [review]})
+                console.log(review)
+            } catch(err){
+                console.log(err)
+            }
+        }
+
+        handleMount()
+    }, [id])
 
 
   
     return (
       <Row className="h-100">
         <Col className="py-2 p-0 p-lg-2" lg={8}>
-
+        <p>Placeholder for reviews</p>
           
           <Container className={appStyles.Content}>
         
-            <p>Placeholder for reviews</p>
+            <p>Placeholder for comments</p>
           </Container>
         </Col>
         <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        
+        <p>Most discussed reviews placeholder</p>
         </Col>
       </Row>
     );
