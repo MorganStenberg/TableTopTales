0982e672476cd3d8c1ba55c41b7b026f3797c58b
Morgan Stenberg <morganstenberg@gmail.com>
2024-04-09 14:54:50 +0000

    add hooks and fetch reviews to reviewspage function

--- src/pages/reviews/ReviewsPage.js
+++ src/pages/reviews/ReviewsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -7,8 +7,29 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/ReviewsPage.module.css";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function ReviewsPage() {
+function ReviewsPage({ message, filter = ""}) {
+
+  const [review, setReviews] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const {data} = await axiosReq.get(`/reviews/?${filter}`)
+        setReviews(data)
+        setHasLoaded(true)
+      } catch(err) {
+        console.log(err)
+      }
+    }
+
+    setHasLoaded(false)
+    fetchReviews()
+  }, [filter, pathname])
   
   return (
     <Row className="h-100">
