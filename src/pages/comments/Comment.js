42fd462ddd9a20263cbcd8f2e6e617de34732ac4
Morgan Stenberg <morganstenberg@gmail.com>
2024-04-10 15:53:52 +0000

    add function for editing comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "../../styles/Comment.module.css"
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -6,6 +6,7 @@ import Avatar from "../../components/Avatar";
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { MoreDropdown } from '../../components/MoreDropDown';
 import { axiosRes } from '../../api/axiosDefaults';
+import CommentEditForm from './CommentEditForm';
 
 // Credit to Code Institute for structure of Comment component
 const Comment = (props) => {
@@ -23,6 +24,7 @@ const Comment = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner; 
+  const [showEditForm, setShowEditForm] = useState(false);
 
   const handleDelete = async () => {
     try {
@@ -56,11 +58,24 @@ const Comment = (props) => {
         <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{created_at}</span>
+          {showEditForm ? (
+           <CommentEditForm
+           id={id}
+           profile_id={profile_id}
+           content={content}
+           profileImage={profile_image}
+           setComments={setComments}
+           setShowEditForm={setShowEditForm}
+         />
+          ) : (
             <p>{content}</p>
-
+          )}
         </Media.Body>
-        {is_owner && (
-          <MoreDropdown handleDelete={handleDelete}/>
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
     </>
