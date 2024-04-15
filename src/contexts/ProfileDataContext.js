c184797ebffde1bc2cfad1279af20d8f40339925
Morgan Stenberg <morganstenberg@gmail.com>
2024-04-15 08:50:46 +0000

    add profiledatacontext, add to index.js

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "./CurrentUserContext";
+
+
+// Credit to Code Institute Walkthrough for ProfileDataProvider
+
+export const ProfileDataContext = createContext();
+export const SetProfileDataContext = createContext();
+
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+    const [profileData, setProfileData] = useState({
+        pageProfile: { results: [] },
+    });
+
+    const currentUser = useCurrentUser();
+
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const { data } = await axiosReq.get(
+                    "/profiles/"
+                );
+                setProfileData((prevState) => ({
+                    ...prevState,
+                    pageProfile: data
+                }));
+            } catch (err) {
+            }
+        };
+        handleMount();
+    }, [currentUser]);
+
+    return (
+        <ProfileDataContext.Provider value={profileData}>
+            <SetProfileDataContext.Provider value={{ setProfileData }}>
+                {children}
+            </SetProfileDataContext.Provider>
+        </ProfileDataContext.Provider>
+    )
+}
