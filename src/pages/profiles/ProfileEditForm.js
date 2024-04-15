91b0a6946d66494110a6ab585c123ca83b0fbba1
Morgan Stenberg <morganstenberg@gmail.com>
2024-04-15 11:30:46 +0000

    add profileeditform, add functions to fetch data

---
+++ src/pages/profiles/ProfileEditForm.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser, useSetCurrentUser } from "../../contexts/CurrentUserContext";
+
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+
+const ProfileEditForm = () => {
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const { id } = useParams();
+  const history = useHistory();
+  const imageFile = useRef();
+
+  const [profileData, setProfileData] = useState({
+    name: "",
+    content: "",
+    image: "",
+    favorite_game: "",
+  });
+  const { name, content, image, favorite_game } = profileData;
+
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    const handleMount = async () => {
+      if (currentUser?.profile_id?.toString() === id) {
+        try {
+          const { data } = await axiosReq.get(`/profiles/${id}/`);
+          const { name, content, image, favorite_game } = data;
+          setProfileData({ name, content, image });
+        } catch (err) {
+          history.push("/");
+        }
+      } else {
+        history.push("/");
+      }
+    };
+
+    handleMount();
+  }, [currentUser, history, id]);
+
+  const handleChange = (event) => {
+    setProfileData({
+      ...profileData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("content", content);
+    formData.append("favorite_game", favorite_game);
+
+    if (imageFile?.current?.files[0]) {
+      formData.append("image", imageFile?.current?.files[0]);
+    }
+
+    try {
+      const { data } = await axiosReq.put(`/profiles/${id}/`, formData);
+      setCurrentUser((currentUser) => ({
+        ...currentUser,
+        profile_image: data.image,
+      }));
+      history.goBack();
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  const textFields = (
+    <>
+
+    </>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      
+    </Form>
+  );
+};
+
+export default ProfileEditForm;
